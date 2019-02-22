cb4b046a4fec13af22453b46a7438ee63067191d
dandelionadia <nadia.ridko@gmail.com>
2019-02-23 00:01:39 +0100

    create table

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,42 @@
 function getTimeList(list) {
   var result = list.reduce(function (acc, obj) {
-    const itemSum = obj.items ? getTimeList(obj.items) : 0
-    return acc.concat(obj.timeHappyPath).concat(itemSum)
+    const items = obj.items ? getTimeList(obj.items) : 0
+    return acc.concat(obj.timeHappyPath).concat(items)
   }, [])
   return result;
 }
 
 function getTotalTime(arr) {
-  var numb = arr.reduce(function (acc, b) {
+  const numb = arr.reduce(function (acc, b) {
     return acc + b
   })
   return numb;
 }
 
+function createTable(arr) {
+  const ul = document.createElement('ul')
+  ul.classList.add('myTable')
+
+  arr.forEach(function (obj) {
+    const li = document.createElement('li')
+    li.classList.add('myTable__row')
+    li.insertAdjacentHTML('beforeend', `
+    <span class="column column__main">${obj.activity}</span>
+    <span class="column column__secondary">${obj.timeHappyPath}</span>
+    <span class="column column__secondary">${obj.timeExpertEstimation}</span>
+    <span class="column column__secondary">${obj.timeWorstCase}</span>
+    `)
+
+    if (obj.items) {
+      const attachment = createTable(obj.items)
+      li.appendChild(attachment)
+    }
+
+    ul.appendChild(li)
+  })
+
+  return ul
+}
 
 fetch('https://private-anon-ce29f4d7f4-urbansim753.apiary-mock.com/priceList', { method: 'GET' })
   .then(function (res) {
@@ -23,4 +47,7 @@ fetch('https://private-anon-ce29f4d7f4-urbansim753.apiary-mock.com/priceList', {
     console.log(totalTime)
 
     document.getElementById('totalTime').innerText = totalTime;
+
+    const html = createTable(data)
+    document.getElementById('table-container').appendChild(html)
   })
