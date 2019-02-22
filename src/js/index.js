87e5e2e9ce315dee01fbcf1a011f003657c3d59c
dandelionadia <nadia.ridko@gmail.com>
2019-02-22 13:21:34 +0100

    get sum total time

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,25 @@
-function renderTree() { }
+function getTimeList(list) {
+  var result = list.reduce(function (acc, obj) {
+    const itemSum = obj.items ? getTimeList(obj.items) : 0
+    return acc.concat(obj.timeHappyPath).concat(itemSum)
+  }, [])
+
+  return result;
+}
+
+function getTotalTime(arr) {
+  var numb = arr.reduce(function (acc, b) {
+    return acc + b
+  })
+  return numb;
+}
+
 
 fetch('https://private-anon-ce29f4d7f4-urbansim753.apiary-mock.com/priceList', { method: 'GET' })
   .then(function (res) {
     return res.json()
   }).then(function (data) {
-    console.log(data)
-    // ...
-    // ...
+    const timeList = getTimeList(data)
+    const totalTime = getTotalTime(timeList)
+    console.log(totalTime)
   })
